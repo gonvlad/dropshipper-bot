9bb8a4f25887dc03641f34a9c810da09a5372fe9
gonvlad <gon.vlad02@gmail.com>
2022-06-05 16:40:05 +0300

    add webhook and redirect fixes

--- bot.py
+++ bot.py
@@ -66,18 +66,23 @@ def handle_main_keyboard(message):
         
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
+def get_message():
    json_string = request.get_data().decode("utf-8")
    update = telebot.types.Update.de_json(json_string)
    bot.process_new_updates([update])
    return "!", 200
 
 
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
 if __name__ == "__main__":
     init_main_keyboard()
     
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
     while True:
